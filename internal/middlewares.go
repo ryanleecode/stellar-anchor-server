cfeb541e99f629a532a08b8d2af4f9b1e6518588
Ryan Lee <drdgvhbh@gmail.com>
2019-07-14 16:13:14 -0400

    Remove API Enveloping
    API Enveloping is not needed and it also makes it difficult to  conform to the stellar protocol spec

--- internal/middlewares.go
+++ internal/middlewares.go
@@ -1,14 +1,8 @@
 package internal
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 	"regexp"
-	"strings"
-
-	"github.com/google/uuid"
-	"github.com/gorilla/mux"
 )
 
 var apiCallPrefix = regexp.MustCompile("/api/v[0-9]+/")
@@ -19,43 +13,4 @@ func ContentType(next http.Handler) http.Handler {
 		writer.Header().Add("Content-Type", "application/json")
 		next.ServeHTTP(writer, request)
 	})
-}
-
-// MethodContext injects the http route, along with its http method in the response
-func MethodContext(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-
-		ctx := context.WithValue(
-			request.Context(),
-			"method",
-			fmt.Sprintf("%s.%s",
-				strings.Replace(
-					apiCallPrefix.ReplaceAllString(
-						request.RequestURI, ""), "/", ".", -1),
-				strings.ToLower(request.Method),
-			)[4:])
-
-		next.ServeHTTP(writer, request.WithContext(ctx))
-	})
-}
-
-// IDContext injects a UUID as the request ID in the response
-func IDContext(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		ctx := context.WithValue(request.Context(), "id", uuid.New().String())
-
-		next.ServeHTTP(writer, request.WithContext(ctx))
-	})
-}
-
-// NewResponseWriter creates a middleware that intercepts another http.ResponseWriter
-func NewResponseWriter(newResponseWriter func(http.ResponseWriter, *http.Request) http.ResponseWriter) mux.MiddlewareFunc {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(
-			func(writer http.ResponseWriter, request *http.Request) {
-				interceptedWriter := newResponseWriter(writer, request)
-
-				next.ServeHTTP(interceptedWriter, request)
-			})
-	}
 }
--- internal/middlewares_test.go
+++ internal/middlewares_test.go
@@ -1,15 +1,11 @@
 package internal_test
 
 import (
-	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
 	"stellar-fi-anchor/internal"
 	"testing"
-
-	"github.com/gorilla/mux"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -31,85 +27,4 @@ func TestContentTypeMiddleware(t *testing.T) {
 	assert.NoError(err)
 
 	assert.EqualValues(res.Header.Get("Content-Type"), "application/json")
-}
-
-func TestRequestIDContextMiddleware(t *testing.T) {
-	mockHandler := func(w http.ResponseWriter, r *http.Request) {
-		id := r.Context().Value("id")
-		fmt.Fprintf(w, "%s", id)
-	}
-
-	assert := assert.New(t)
-
-	handler := internal.IDContext(http.HandlerFunc(mockHandler))
-	ts := httptest.NewServer(handler)
-	defer ts.Close()
-
-	res, err := http.Get(ts.URL)
-	assert.NoError(err)
-	body, err := ioutil.ReadAll(res.Body)
-	assert.NoError(err)
-
-	id := string(body)
-
-	assert.Regexp(uuidRegex, id)
-}
-
-func TestRequestMethodContextMiddleware(t *testing.T) {
-	mockHandler := func(w http.ResponseWriter, r *http.Request) {
-		method := r.Context().Value("method")
-		fmt.Fprintf(w, "%s", method)
-	}
-
-	assert := assert.New(t)
-
-	const apiVersion = "v1"
-	const route = "testing"
-
-	handler := internal.MethodContext(
-		http.HandlerFunc(mockHandler))
-	router := mux.NewRouter().PathPrefix(
-		fmt.Sprintf("/%s", apiVersion)).Subrouter()
-	router.HandleFunc(fmt.Sprintf("/%s", route), handler.ServeHTTP)
-	ts := httptest.NewServer(router)
-	defer ts.Close()
-
-	res, err := http.Get(fmt.Sprintf("%s/%s/%s", ts.URL, apiVersion, route))
-	assert.NoError(err)
-	body, err := ioutil.ReadAll(res.Body)
-	assert.NoError(err)
-
-	method := string(body)
-
-	assert.EqualValues(fmt.Sprintf("%s.get", route), method)
-}
-
-func TestResponseWriterMiddleware(t *testing.T) {
-	testMessage := "testing"
-	mockHandler := func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, testMessage)
-	}
-
-	writer := httptest.NewRecorder()
-	writerFactory := func(http.ResponseWriter, *http.Request) http.ResponseWriter {
-		return writer
-	}
-
-	assert := assert.New(t)
-
-	handler := internal.NewResponseWriter(
-		writerFactory)(http.HandlerFunc(mockHandler))
-	ts := httptest.NewServer(handler)
-	defer ts.Close()
-
-	_, err := http.Get(ts.URL)
-	assert.NoError(err)
-
-	resp := writer.Result()
-	body, err := ioutil.ReadAll(resp.Body)
-	assert.NoError(err)
-
-	message := string(body)
-
-	assert.EqualValues(testMessage, message)
 }
