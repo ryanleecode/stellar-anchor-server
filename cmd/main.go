7c997e12e556a412cc3dea677478203e8548d9a6
Ryan Lee <drdgvhbh@gmail.com>
2019-07-19 18:12:09 -0400

    Rename environment variable

--- cmd/main.go
+++ cmd/main.go
@@ -1,54 +1,15 @@
-// Package stellaranchor Stellar FI Anchor Emulator API.
-//
-// FI Anchor Emulator for the Stellar Network
-//
-//     Schemes: http, https
-//     Host: localhost
-//     BasePath: /v1
-//     Version: 0.0.2
-//     License: MIT http://opensource.org/licenses/MIT
-//     Contact: Ryan Lee<ryanleecode@gmail.com> http://drdgvhbh.io
-//
-//     Consumes:
-//     - application/json
-//
-//     Produces:
-//     - application/json
-//
-//     Security:
-//     - api_key:
-//
-//     SecurityDefinitions:
-//     api_key:
-//          type: apiKey
-//          name: Authorization
-//          in: header
-//
-// swagger:meta
 package main
 
 import (
-	"context"
-	"crypto/rand"
 	"fmt"
-	hdwallet "github.com/drdgvhbh/go-ethereum-hdwallet"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/jinzhu/gorm"
 	log "github.com/sirupsen/logrus"
-	"github.com/stellar/go/keypair"
-	"github.com/stellar/go/network"
 	"net/http"
 	"os"
-	"stellar-fi-anchor/internal/accounts"
-	"stellar-fi-anchor/internal/asset"
-	"stellar-fi-anchor/internal/authentication"
-	"stellar-fi-anchor/internal/random"
-	stellarsdk "stellar-fi-anchor/internal/stellar-sdk"
+	"stellar-fi-anchor/internal"
 	"time"
 
-	"github.com/ethereum/go-ethereum/rpc"
 	_ "github.com/joho/godotenv/autoload"
-	"stellar-fi-anchor/internal"
 )
 
 func main() {
@@ -60,21 +21,10 @@ func main() {
 	if !ok {
 		log.Fatalln("env variable PRIVATE_KEY not defined")
 	}
-	passphrase := network.TestNetworkPassphrase
-	fiKeyPair, err := keypair.Parse(privateKey)
-	if err != nil {
-		log.Fatalln("private key is not parsable")
-	}
-	ethMnemonic, ok := os.LookupEnv("ETHEREUM_MNEMONIC")
+	mnemonic, ok := os.LookupEnv("MNEMONIC")
 	if !ok {
-		log.Fatalln("env variable ETHEREUM_MNEMONIC not defined")
+		log.Fatalln("env variable MNEMONIC not defined")
 	}
-	wallet, err := hdwallet.NewFromMnemonic(ethMnemonic)
-	if err != nil {
-		log.Fatalln("cannot create accounts wallet")
-	}
-	print(wallet)
-
 	db, err := gorm.Open(
 		"postgres", "host=localhost port=6666 user=postgres dbname=postgres sslmode=disable")
 	if err != nil {
@@ -84,30 +34,7 @@ func main() {
 		_ = db.Close()
 	}()
 
-	ipcClient, err := rpc.DialIPC(context.TODO(), "/home/drd/.ethereum/goerli/geth.ipc")
-	if err != nil {
-		log.Fatal(err)
-	}
-	ethIpcClient := ethclient.NewClient(ipcClient)
-	chainId, err := ethIpcClient.ChainID(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-	println(chainId.String())
-
-	db.AutoMigrate(asset.Asset{}, accounts.GenericAccount{})
-
-	accountServices := []internal.AccountService{accounts.NewService(wallet, db)}
-
-	challengeTxFact := stellarsdk.NewChallengeTransactionFactory(
-		passphrase,
-		func() (s string, e error) {
-			return random.NewGenerateString(random.NewGenerateBytes(rand.Read))(48)
-		})
-	authService := authentication.NewService(
-		challengeTxFact, fiKeyPair.(*keypair.Full), passphrase)
-
-	rootHandler := internal.NewRootHandler(authService, accountServices)
+	rootHandler := internal.Bootstrap(privateKey, mnemonic, db)
 
 	server := &http.Server{
 		Handler:      rootHandler,
---
+++ internal/bootstrap.go
@@ -0,0 +1,52 @@
+package internal
+
+import (
+	"context"
+	"crypto/rand"
+	hdwallet "github.com/drdgvhbh/go-ethereum-hdwallet"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/jinzhu/gorm"
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
+	"log"
+	"net/http"
+	"stellar-fi-anchor/internal/accounts"
+	"stellar-fi-anchor/internal/asset"
+	"stellar-fi-anchor/internal/authentication"
+	"stellar-fi-anchor/internal/random"
+	stellarsdk "stellar-fi-anchor/internal/stellar-sdk"
+)
+
+func Bootstrap(privateKey string, mnemonic string, db *gorm.DB) http.Handler {
+	wallet, err := hdwallet.NewFromMnemonic(mnemonic)
+	if err != nil {
+		log.Fatalln("cannot create accounts wallet")
+	}
+	passphrase := network.TestNetworkPassphrase
+	fiKeyPair, err := keypair.Parse(privateKey)
+	if err != nil {
+		log.Fatalln("private key is not parsable")
+	}
+
+	ipcClient, err := rpc.DialIPC(context.TODO(), "/home/drd/.ethereum/goerli/geth.ipc")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ethIpcClient := ethclient.NewClient(ipcClient)
+	ethIpcClient.ChainID(context.TODO()) // TODO
+
+	db.AutoMigrate(asset.Asset{}, accounts.GenericAccount{})
+
+	accountServices := []AccountService{accounts.NewService(wallet, db)}
+
+	challengeTxFact := stellarsdk.NewChallengeTransactionFactory(
+		passphrase,
+		func() (s string, e error) {
+			return random.NewGenerateString(random.NewGenerateBytes(rand.Read))(48)
+		})
+	authService := authentication.NewService(
+		challengeTxFact, fiKeyPair.(*keypair.Full), passphrase)
+
+	return NewRootHandler(authService, accountServices)
+}
