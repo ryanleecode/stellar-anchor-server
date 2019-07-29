b5354f2f274224cfb31e674498d5acfd97c3d13b
Ryan Lee <drdgvhbh@gmail.com>
2019-07-28 23:23:10 -0400

    Fix broken blockchain test
    Implements the mock methods necessary for EthClient to make the blockchain test compile.

--- ethereum/mock/eth_client.go
+++ ethereum/mock/eth_client.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
+
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/mock"
 )
@@ -22,4 +24,16 @@ func (c *EthClient) HeaderByNumber(ctx context.Context, number *big.Int) (*types
 	args := c.Called(ctx, number)
 
 	return args.Get(0).(*types.Header), args.Error(1)
+}
+
+func (c *EthClient) TransactionByHash(ctx context.Context, hash common.Hash) (tx *types.Transaction, isPending bool, err error) {
+	args := c.Called(ctx, hash)
+
+	return args.Get(0).(*types.Transaction), args.Bool(1), args.Error(2)
+}
+
+func (c *EthClient) TransactionReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error) {
+	args := c.Called(ctx, txHash)
+
+	return args.Get(0).(*types.Receipt), args.Error(2)
 }
